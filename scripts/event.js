2c74770e9f834755f9d7ce39e07b3c68de00e73c
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-08 19:51:19 -0400

    did several changes

--- scripts/event.js
+++ scripts/event.js
@@ -1,44 +1,15 @@
-// Define your events here
-const events = [
-    {
-      date: '2023-06-12',
-      title: 'Meeting with clients',
-      description: 'Discuss project requirements and timelines',
-    },
-    {
-      date: '2023-06-18',
-      title: 'Team outing',
-      description: 'Enjoy a fun-filled day with colleagues',
-    },
-    // Add more events as needed
-  ];
-  
-  // Function to add events to the table
-  function addEventsToTable() {
-    const tableBody = document.querySelector('#eventCalendar tbody');
-  
-    // Clear table body
-    tableBody.innerHTML = '';
-  
-    // Add events to the table
-    events.forEach((event) => {
-      const row = document.createElement('tr');
-      const dateCell = document.createElement('td');
-      const titleCell = document.createElement('td');
-      const descriptionCell = document.createElement('td');
-  
-      dateCell.textContent = event.date;
-      titleCell.textContent = event.title;
-      descriptionCell.textContent = event.description;
-  
-      row.appendChild(dateCell);
-      row.appendChild(titleCell);
-      row.appendChild(descriptionCell);
-  
-      tableBody.appendChild(row);
-    });
-  }
-  
-  // Call the function to add events to the table
-  addEventsToTable();
-  
+"use strict";
+
+      // Event data
+      var eventDates = ["July 29, 2018 11:00:00", "July 30, 2018 19:00:00", "July 31, 2018 19:30:00",
+                        // Rest of the event dates...
+                        "September 13, 2018 20:00:00"];
+      var eventDescriptions = ["Classics Brunch", "Lasers and Light", "Dixieland Jazz Masters",
+                               // Rest of the event descriptions...
+                               "Antonio Perez"];
+      var eventPrices = ["$12", "$12/$18/$24", "$22/$31/$47",
+                         // Rest of the event prices...
+                         "$32/$48/$64"];
+
+      // Create calendar view
+      createCalendar("calendar", eventDates, eventDescriptions, eventPrices);
---
+++ scripts/productData.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        name: "Creme Brulee",
+        image: "images/fr_creme.jpg",
+        price: 10
+    },
+    {
+        name: "Japanese Cheesecake",
+        image: "images/jp_cheese.jpg",
+        price: 15
+    },
+    {
+        name: "Bingsu",
+        image: "images/bingsu.jpg",
+        price: 17
+    },
+      {
+        name: "Japanese Cheesecake",
+        image: "images/jp_cheese.jpg",
+        price: 20
+    },
+      {
+        name: "Japanese Cheesecake",
+        image: "images/jp_cheese.jpg",
+        price: 20
+    },
+      {
+        name: "Japanese Cheesecake",
+        image: "images/jp_cheese.jpg",
+        price: 20
+    }
+];
