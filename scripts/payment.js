682956d6fecc972bc53f8ea99989fd751c822e2d
AliAngelinaaa <nanzum1140@gmail.com>
2023-07-09 20:51:27 -0400

    the shop works

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,116 +1,112 @@
 window.addEventListener("load", function () {
-    // Retrieve the field/value pairs from the URL
-    var orderData = location.search.slice(1);
-    orderData = orderData.replace(/\+/g, " ");
-    orderData = decodeURIComponent(orderData);
-    var orderFields = orderData.split(/[&=]/g);
-  
-    // Write the field values to the order form
-    document.forms.order.elements.modelName.value = orderFields[3];
-    document.forms.order.elements.modelQty.value = orderFields[5];
-    document.forms.order.elements.orderCost.value = orderFields[7];
-    document.forms.order.elements.shippingType.value = orderFields[9];
-    document.forms.order.elements.shippingCost.value = orderFields[13];
-    document.forms.order.elements.subTotal.value = orderFields[15];
-    document.forms.order.elements.salesTax.value = orderFields[17];
-    document.forms.order.elements.cartTotal.value = orderFields[19];
+  var selectedItems = sessionStorage.getItem("selectedItems");
+  var totalAmount = sessionStorage.getItem("totalAmount");
+
+  document.getElementById("selectedItems").innerText = selectedItems;
+  document.getElementById("totalAmount").innerText = totalAmount;
+
+  document.getElementById("paymentForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Perform payment processing logic here
+    alert("Payment successful!");
+    window.location.href = "verification.html";
   });
-  
-  window.addEventListener("load", function () {
-    document.getElementById("subButton").onclick = runSubmit;
-    document.getElementById("name").oninput = validateName;
-    document.getElementById("cardNumber").oninput = validateNumber;
-    document.getElementById("expiration-date").oninput = validateDate;
-    document.getElementById("cvc").oninput = validateCVC;
-  });
-  
-  function runSubmit() {
-    validateName();
-    validateCredit();
-    validateNumber();
-    validateDate();
-    validateCVC();
+});
+
+window.addEventListener("load", function () {
+  document.getElementById("subButton").onclick = runSubmit;
+  document.getElementById("name").oninput = validateName;
+  document.getElementById("cardNumber").oninput = validateNumber;
+  document.getElementById("expiration-date").oninput = validateDate;
+  document.getElementById("cvc").oninput = validateCVC;
+});
+
+function runSubmit() {
+  validateName();
+  validateCredit();
+  validateNumber();
+  validateDate();
+  validateCVC();
+}
+
+function validateDate() {
+  var cardDate = document.getElementById("expDate");
+  if (cardDate.validity.valueMissing) {
+    cardDate.setCustomValidity("Enter the expiration date");
+  } else if (/^(0[1-9]|1[0-2])\/20[12]\d$/.test(cardDate.value) === false) {
+    cardDate.setCustomValidity("Enter a valid expiration date");
+  } else {
+    cardDate.setCustomValidity("");
   }
-  
-  function validateDate() {
-    var cardDate = document.getElementById("expDate");
-    if (cardDate.validity.valueMissing) {
-      cardDate.setCustomValidity("Enter the expiration date");
-    } else if (/^(0[1-9]|1[0-2])\/20[12]\d$/.test(cardDate.value) === false) {
-      cardDate.setCustomValidity("Enter a valid expiration date");
-    } else {
-      cardDate.setCustomValidity("");
-    }
+}
+
+function validateName() {
+  var cardName = document.getElementById("name");
+  if (cardName.validity.valueMissing) {
+    cardName.setCustomValidity("Please enter your name");
+  } else {
+    cardName.setCustomValidity("");
   }
-  
-  function validateName() {
-    var cardName = document.getElementById("name");
-    if (cardName.validity.valueMissing) {
-      cardName.setCustomValidity("Please enter your name");
-    } else {
-      cardName.setCustomValidity("");
-    }
+}
+
+function validateCredit() {
+  var creditCard = document.forms.credit.elements.company[0];
+  if (creditCard.validity.valueMissing) {
+    creditCard.setCustomValidity("Select the credit card you're using");
+  } else {
+    creditCard.setCustomValidity("");
   }
-  
-  function validateCredit() {
-    var creditCard = document.forms.credit.elements.company[0];
-    if (creditCard.validity.valueMissing) {
-      creditCard.setCustomValidity("Select the credit card you're using");
-    } else {
-      creditCard.setCustomValidity("");
-    }
+}
+
+function validateNumber() {
+  var cardNumber = document.getElementById("cardNumber");
+  if (cardNumber.validity.valueMissing) {
+    cardNumber.setCustomValidity("Enter your card number");
+  } else if (cardNumber.validity.patternMismatch) {
+    cardNumber.setCustomValidity("Enter a valid card number");
+  } else if (luhn(cardNumber.value) === false) {
+    cardNumber.setCustomValidity("Enter a legitimate card number");
+  } else {
+    cardNumber.setCustomValidity("");
   }
-  
-  function validateNumber() {
-    var cardNumber = document.getElementById("cardNumber");
-    if (cardNumber.validity.valueMissing) {
-      cardNumber.setCustomValidity("Enter your card number");
-    } else if (cardNumber.validity.patternMismatch) {
-      cardNumber.setCustomValidity("Enter a valid card number");
-    } else if (luhn(cardNumber.value) === false) {
-      cardNumber.setCustomValidity("Enter a legitimate card number");
-    } else {
-      cardNumber.setCustomValidity("");
-    }
+}
+
+function validateCVC() {
+  var cardCVC = document.getElementById("cvc");
+  var creditCard = document.querySelector('input[name="company"]:checked').value;
+
+  if (cardCVC.validity.valueMissing) {
+    cardCVC.setCustomValidity("Enter your code CVC number");
+  } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+    cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+  } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+    cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+  } else {
+    cardCVC.setCustomValidity("");
   }
-  
-  function validateCVC() {
-    var cardCVC = document.getElementById("cvc");
-    var creditCard = document.querySelector('input[name="company"]:checked').value;
-  
-    if (cardCVC.validity.valueMissing) {
-      cardCVC.setCustomValidity("Enter your code CVC number");
-    } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
-      cardCVC.setCustomValidity("Enter a 4-digit CVC number");
-    } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
-      cardCVC.setCustomValidity("Enter a 3-digit CVC number");
-    } else {
-      cardCVC.setCustomValidity("");
-    }
+}
+
+function sumDigits(numStr) {
+  var digitTotal = 0;
+  for (var i = 0; i < numStr.length; i++) {
+    digitTotal += parseInt(numStr.charAt(i));
   }
-  
-  function sumDigits(numStr) {
-    var digitTotal = 0;
-    for (var i = 0; i < numStr.length; i++) {
-      digitTotal += parseInt(numStr.charAt(i));
-    }
-    return digitTotal;
+  return digitTotal;
+}
+
+function luhn(idNum) {
+  var string1 = "";
+  var string2 = "";
+
+  // Retrieve the odd-numbered digits
+  for (var i = idNum.length - 1; i >= 0; i -= 2) {
+    string1 += idNum.charAt(i);
   }
-  
-  function luhn(idNum) {
-    var string1 = "";
-    var string2 = "";
-  
-    // Retrieve the odd-numbered digits
-    for (var i = idNum.length - 1; i >= 0; i -= 2) {
-      string1 += idNum.charAt(i);
-    }
-    // Retrieve the even-numbered digits and double them
-    for (var i = idNum.length - 2; i >= 0; i -= 2) {
-      string2 += 2 * idNum.charAt(i);
-    }
-  
-    // Return whether the sum of the digits is divisible by 10
-    return sumDigits(string1 + string2) % 10 === 0;
+  // Retrieve the even-numbered digits and double them
+  for (var i = idNum.length - 2; i >= 0; i -= 2) {
+    string2 += 2 * idNum.charAt(i);
   }
-  
+
+  // Return whether the sum of the digits is divisible by 10
+  return sumDigits(string1 + string2) % 10 === 0;
+}
--- scripts/shop.js
+++ scripts/shop.js
@@ -18,106 +18,65 @@ if (loginBtn) {
   });
 }
 
-// Get the item grid element
-const itemGrid = document.getElementById("item-grid");
-
-// Generate HTML elements for each product
-products.forEach(product => {
-  const itemCard = document.createElement("div");
-  itemCard.className = "item-card";
-
-  const image = document.createElement("img");
-  image.src = product.image;
-  image.alt = product.name;
-
-  const label = document.createElement("label");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.name = "item";
-  checkbox.value = product.price;
-  checkbox.className = "item-checkbox";
-  const name = document.createElement("h3");
-  name.textContent = product.name;
-  const price = document.createElement("p");
-  price.textContent = `$${product.price}`;
-
-  label.appendChild(checkbox);
-  label.appendChild(name);
-  label.appendChild(price);
-
-  itemCard.appendChild(image);
-  itemCard.appendChild(label);
-
-  itemGrid.appendChild(itemCard);
-
-  // Add event listener to the checkbox
-  checkbox.addEventListener("change", updateTotal);
-});
-
-// Retrieve the item cards
-var itemCards = document.getElementsByClassName("item-card");
-
-// Initialize the total price, tax, and total amount
-var totalPrice = 0;
-var tax = 0;
-var totalAmount = 0;
-
-// Function to update the total price, tax, and total amount
-function updateTotal() {
-  // Reset the values
-  totalPrice = 0;
-  tax = 0;
-  totalAmount = 0;
-
-  // Iterate over the item cards and calculate the total price
-  for (var i = 0; i < itemCards.length; i++) {
-    var checkbox = itemCards[i].querySelector(".item-checkbox");
-    if (checkbox.checked) {
-      var price = parseFloat(checkbox.value);
-      totalPrice += price;
-    }
+function calculateTotal() {
+  var selectedItems = [];
+  var totalAmount = 0;
+  
+  var checkboxes = document.getElementsByName("item");
+  for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+          selectedItems.push(checkboxes[i].name);
+              totalAmount += parseInt(checkboxes[i].value);
+      }
   }
-
-  // Calculate the tax and total amount
-  tax = totalPrice * 0.08; // Assuming 8% tax rate
-  totalAmount = totalPrice + tax;
-
-  // Update the HTML elements with the calculated values
-  var priceElement = document.getElementById("price");
-  var taxElement = document.getElementById("tax");
-  var totalElement = document.getElementById("total");
-
-  priceElement.textContent = "$" + totalPrice.toFixed(2);
-  taxElement.textContent = "$" + tax.toFixed(2);
-  totalElement.textContent = "$" + totalAmount.toFixed(2);
-
-  // Store the total amount in localStorage
-  localStorage.setItem("totalAmount", totalAmount.toFixed(2));
-
-  // Store the selected items in localStorage
-  var selectedItems = "";
-  for (var i = 0; i < itemCards.length; i++) {
-    var checkbox = itemCards[i].querySelector(".item-checkbox");
-    if (checkbox.checked) {
-      var itemName = itemCards[i].querySelector("h3").textContent;
-      selectedItems += itemName + ", ";
-    }
-  }
-  localStorage.setItem("selectedItems", selectedItems);
-
-  // Display the selected items in the payment.html page
-  var selectedItemsElement = document.getElementById("selected-items");
-  if (selectedItemsElement) {
-    selectedItemsElement.textContent = selectedItems;
-  }
+  
+  var tax = totalAmount * 0.08;
+  var totalPrice = totalAmount + tax;
+  
+  document.getElementById("selectedItems").innerText = "Selected Item(s):" + selectedItems.join(", ");
+  document.getElementById("taxNum").innerText = "Tax: $" + tax.toFixed(2);
+  document.getElementById("totalAmount").innerText = "Total Amount: $" + totalPrice.toFixed(2);
+  
+  // Store the order details in sessionStorage
+  sessionStorage.setItem("selectedItems", selectedItems.join(", "));
+  sessionStorage.setItem("totalAmount", totalPrice.toFixed(2));
 }
 
-// Add event listener to the Add to Cart button
-var addToCartBtn = document.getElementById("add-cart-btn");
-addToCartBtn.addEventListener("click", function (event) {
-  // Prevent the default form submission
+document.getElementById("storeForm").addEventListener("submit", function(event) {
   event.preventDefault();
-
-  // Redirect to the payment.html page
   window.location.href = "payment.html";
 });
+
+
+// function calculateTotal() {
+//   var selectedItems = [];
+//   var totalAmount = 0;
+  
+//   var checkboxes = document.getElementsByName("item");
+//   for (var i = 0; i < checkboxes.length; i++) {
+//       if (checkboxes[i].checked) {
+//           selectedItems.push(checkboxes[i].value);
+//           if (checkboxes[i].value === "item1") {
+//               totalAmount += 10;
+//           } else if (checkboxes[i].value === "item2") {
+//               totalAmount += 15;
+//           }
+//       }
+//   }
+  
+//   var tax = totalAmount * 0.08;
+//   var totalPrice = totalAmount + tax;
+  
+//   document.getElementById("selectedItems").innerText = "Selected Item(s):" + selectedItems.join(", ");
+//   document.getElementById("taxNum").innerText = "Tax: $" + tax.toFixed(2);
+//   document.getElementById("totalAmount").innerText = "Total Amount: $" + totalPrice.toFixed(2);
+  
+//   // Store the order details in sessionStorage
+//   sessionStorage.setItem("selectedItems", selectedItems.join(", "));
+//   sessionStorage.setItem("totalAmount", totalPrice.toFixed(2));
+// }
+
+// document.getElementById("storeForm").addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   window.location.href = "payment.html";
+// });
